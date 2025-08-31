50ea0c822f51fe755b7a28e70e064bd7997c908a
Tiago Borges <103573703+TFX19@users.noreply.github.com>
2025-08-31 13:08:58 +0100

    Project "Links", React native fundamentals

---
+++ src/app/index/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import {styles} from "./styles"
+import { MaterialIcons } from "@expo/vector-icons"
+import { colors } from "@/styles/colors";
+import { Category } from "@/components/category";
+
+export default function Index() {
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image source={require("@/assets/logo.png")} style={styles.logo} />
+          
+          <TouchableOpacity>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+          </TouchableOpacity>
+        </View>
+        <Category />
+      </View>
+    </>
+  );
+}
+
