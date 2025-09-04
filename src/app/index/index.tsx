a15489d51bc26d38e6e4303c134c8401e6ede440
Tiago Borges <103573703+TFX19@users.noreply.github.com>
2025-09-04 11:48:32 +0100

    ending navigation and states

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Text,
   View,
@@ -6,26 +7,31 @@ import {
   FlatList,
   Modal,
 } from "react-native";
+import { router } from "expo-router";
 import { styles } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 
 import { Categories } from "@/components/categories";
+import { categories } from "@/utils/categories";
 import { Link } from "@/components/link";
 import { Option } from "@/components/option";
 export default function Index() {
+
+    const [category, setCategory] = useState(categories[0].name)
+
   return (
     <>
       <View style={styles.container}>
         <View style={styles.header}>
           <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => router.navigate("/add")}>
             <MaterialIcons name="add" size={32} color={colors.green[300]} />
           </TouchableOpacity>
         </View>
 
-        <Categories />
+        <Categories onChange={setCategory} selected={category} />
 
         <FlatList
           data={["1", "2", "3", "4"]}
@@ -42,24 +48,24 @@ export default function Index() {
           showsVerticalScrollIndicator={false}
         />
 
-        <Modal transparent visible={true}>
+        <Modal transparent visible={false}>
           <View style={styles.modal}>
             <View style={styles.modalContent}>
               <View style={styles.modalHeader}>
                 <Text style={styles.modalCategory}>Curso</Text>
                 <TouchableOpacity>
-                <MaterialIcons
-                  name="close"
-                  size={20}
-                  color={colors.gray[400]}
-                />
+                  <MaterialIcons
+                    name="close"
+                    size={20}
+                    color={colors.gray[400]}
+                  />
                 </TouchableOpacity>
               </View>
               <Text style={styles.modalLinkName}>tiago</Text>
               <Text style={styles.modalUrl}>https://www.tiagob.online</Text>
 
               <View style={styles.modalFooter}>
-                <Option name="Excluir" icon="delete" variant="secondary"/>
+                <Option name="Excluir" icon="delete" variant="secondary" />
                 <Option name="Abrir" icon="language" />
               </View>
             </View>
